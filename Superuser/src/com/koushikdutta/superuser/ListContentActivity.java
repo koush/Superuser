c585e7159f33b8792e19902be748ba3c617f47ea
Koushik Dutta <koushd@gmail.com>
2013-02-19 13:35:03 -0800

    wip

---
+++ Superuser/src/com/koushikdutta/superuser/ListContentActivity.java
@@ -0,0 +1,7 @@
+package com.koushikdutta.superuser;
+
+import android.support.v4.app.FragmentActivity;
+
+public class ListContentActivity extends FragmentActivity {
+
+}
--- Superuser/src/com/koushikdutta/superuser/MainActivity.java
+++ Superuser/src/com/koushikdutta/superuser/MainActivity.java
@@ -1,7 +1,20 @@
 package com.koushikdutta.superuser;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
 }
