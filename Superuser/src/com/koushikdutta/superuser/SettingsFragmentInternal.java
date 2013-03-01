6148624a64ac88aa81a96cc7e7b379efc0be3ffe
Koushik Dutta <koushd@gmail.com>
2013-02-28 23:35:51 -0800

    LICENSE
    Change-Id: Ib20b85d3a447a42c1f7b3c0ff0203f314b909ae3

--- Superuser/src/com/koushikdutta/superuser/SettingsFragmentInternal.java
+++ Superuser/src/com/koushikdutta/superuser/SettingsFragmentInternal.java
@@ -1,18 +1,30 @@
+/*
+ * Copyright (C) 2013 Koushik Dutta (@koush)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.koushikdutta.superuser;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.koushikdutta.superuser.util.Settings;
