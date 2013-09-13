93f3dfccc5b53de87c67da38559f3c4faa85ebb1
BILLyTheLiTTle <littleprog@gmail.com>
2013-09-13 14:42:48 +0300

    Several fixes after setting up correctly my repo

--- Superuser/src/com/koushikdutta/superuser/util/SuHelper.java
+++ Superuser/src/com/koushikdutta/superuser/util/SuHelper.java
@@ -7,25 +7,21 @@ import android.util.Log;
 
 public final class SuHelper {
     public static String CURRENT_VERSION = "13";
-    public static void checkSu(Context context) throws Exception {
-=======
-public final class SuHelper {
-    public static final String CURRENT_VERSION = "9";
     public static void checkSu(Context context) throws IllegalResultException, 
-    IllegalBinaryException, Exception {
-	Process p = Runtime.getRuntime().exec("su -v");
-        String result = Settings.readToEnd(p.getInputStream());
-        Log.i("Superuser", "Result: " + result);
-        final int exitValue = p.waitFor();
-        if (exitValue != 0)
-            throw new IllegalResultException("Expected zero (0) but returned "+exitValue+".");
-        if (result == null)
-            throw new IllegalResultException("Empty result returned.");
-        if (!result.contains(context.getPackageName()))
-            throw new IllegalBinaryException("The su binary is not the appropriate for this app.");
-        
-        String[] parts = result.split(" ");
-        if (!CURRENT_VERSION.equals(parts[0]))
-            throw new IllegalBinaryException("Su binary is out of date.");
-    }
+	    IllegalBinaryException, Exception {
+		Process p = Runtime.getRuntime().exec("su -v");
+	        String result = Settings.readToEnd(p.getInputStream());
+	        Log.i("Superuser", "Result: " + result);
+	        final int exitValue = p.waitFor();
+	        if (exitValue != 0)
+	            throw new IllegalResultException("Expected zero (0) but returned "+exitValue+".");
+	        if (result == null)
+	            throw new IllegalResultException("Empty result returned.");
+	        if (!result.contains(context.getPackageName()))
+	            throw new IllegalBinaryException("The su binary is not the appropriate for this app.");
+	        
+	        String[] parts = result.split(" ");
+	        if (!CURRENT_VERSION.equals(parts[0]))
+	            throw new IllegalBinaryException("Su binary is out of date.");
+	    }
 }
