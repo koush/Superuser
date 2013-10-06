63afa5ab908f138afafb61a1e034e833ed16151f
Koushik Dutta <koushd@gmail.com>
2013-10-05 22:36:53 -0700

    Only use access_disabled if SUPERUSER_EMBEDDED.
    It is not possible to reenable Superuser when non embedded. su will reject.
    
    Change-Id: If440d9d758c21c61a2d7c0bef132eb716b494c5d

--- Superuser/jni/su/su.c
+++ Superuser/jni/su/su.c
@@ -559,6 +559,9 @@ static __attribute__ ((noreturn)) void allow(struct su_context *ctx) {
  * Find the properties ourselves.
  */
 int access_disabled(const struct su_initiator *from) {
+#ifndef SUPERUSER_EMBEDDED
+    return 0;
+#else
     char *data;
     char build_type[PROPERTY_VALUE_MAX];
     char debuggable[PROPERTY_VALUE_MAX], enabled[PROPERTY_VALUE_MAX];
@@ -608,6 +611,7 @@ int access_disabled(const struct su_initiator *from) {
 
     }
     return 0;
+#endif
 }
 
 static int is_api_18() {
