d772146915ea34ddb34346ca5d94aac9a8bc5132
Pierre-Hugues Husson <husson@archos.com>
2015-10-03 00:42:24 +0200

    Add a build-zip.sh script to generate update.zip

---
+++ Superuser/build-zip.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+tmpdir="$(mktemp -d)"
+ORIG="$(pwd)"
+
+pushd "$tmpdir"
+mkdir -p META-INF/com/google/android
+cp "$ORIG"/assets/update-binary META-INF/com/google/android/update-binary
+cp "$ORIG"/assets/install-recovery.sh .
+cp -R "$ORIG"/libs/{x86,mips,armeabi} .
+
+zip -r "$ORIG"/update-su.zip *
+popd
+rm -Rf "$tmpdir"
