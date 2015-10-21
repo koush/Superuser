dafc04d41296c86ef1530b05d730dc344a9b8773
Pierre-Hugues Husson <husson@archos.com>
2015-10-21 21:54:55 +0200

    Bye bye daemon (cf issue #4)

--- Superuser/jni/su/su.h
+++ Superuser/jni/su/su.h
@@ -176,7 +176,6 @@ void exec_logd(const char* fmt, ...);
 int run_daemon();
 int connect_daemon(int argc, char *argv[], int ppid);
 int su_main(int argc, char *argv[]);
-int su_main_nodaemon(int argc, char *argv[]);
 // for when you give zero fucks about the state of the child process.
 // this version of fork understands you don't care about the child.
 // deadbeat dad fork.
